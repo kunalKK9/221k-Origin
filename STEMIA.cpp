fbabf9e8f3b3529f53e2f58461d2b48f5eb115de
KunalK <143835289+kunalKK9@users.noreply.github.com>
2025-12-06 00:19:12 -0700

    Add robot long goal type shi
    This file contains the implementation of the robot's control system, including motor configurations, sensor setups, and autonomous behavior. stupid copilot ai summary

---
+++ STEMIA.cpp
@@ -0,0 +1,523 @@
+#include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/abstract_motor.hpp"
+#include "pros/adi.hpp"
+#include "pros/misc.h"
+#include "pros/motors.hpp"
+#include "pros/optical.hpp"
+#include "pros/rotation.hpp"
+#include "pros/rtos.hpp"
+#include <cerrno>
+
+// currently playing silksong so zero coding will get done 
+
+/**
+TROUBLESHOOTING:
+IF CODE WILL NOT UPLOAD :
+- CRTL - S, Clean, Build, Upload.
+
+IF EXIT CODE 2:
+- Get new wire or upload directly to brain, ports fried or wire fried.
+*/
+
+// add a nope mech 
+
+// controller
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+
+// motor groups
+pros::MotorGroup leftMotors({1, 2, -3}, pros::MotorGearset::blue); // left motor group - ports 1 2 3
+pros::MotorGroup rightMotors({-8, -9, 10}, pros::MotorGearset::blue); // right motor group - ports 4 5 6
+
+// optical sensor for alliance colour detection
+pros::Optical allianceSensor(17); // optical sensor on port 17
+
+// ---- PISTONS ----
+// Scraper piston and nope toggle
+pros::adi::DigitalOut scraper('A');
+// true  -> scraper DOWN
+// false -> scraper UP
+bool scraperDown = false;  
+
+pros::adi::DigitalOut nope('B');
+// true  -> nope UP (non-blocking)
+// false -> nope DOWN (blocking)
+bool nopeToggle = true;
+
+pros::adi::DigitalOut descore('C');
+bool descoretoggle = false;
+
+pros::adi::DigitalOut indexer('D');
+bool indexertoggle = true;
+
+pros::adi::DigitalOut coloursort('E');
+bool coloursorttoggle = true;
+
+//nope functions
+
+//MOTORS:
+
+//rubber band intake motor
+pros::Motor intake(18, pros::MotorGearset::green); // 11W motor
+
+//indexer / top roller motors
+pros::Motor Indexermotor(13, pros::MotorGearset::blue); //11W motor
+
+//Sensors:
+pros::Optical longgoaloptical(19);
+bool autoOrangeActive = false;   // true when sensor is overriding the nope
+
+
+// tracking wheels + inertial sensors
+
+// Inertial Sensor on port 10
+pros::Imu imu(12);
+// horizontal tracking wheel encoder. Rotation sensor, port 20, not reversed (will change later in testing)
+pros::Rotation horizontalEnc(-20);
+// vertical tracking wheel encoder. Rotation sensor, port 11, reversed (will change later in testing)
+pros::Rotation verticalEnc(-11);
+// horizontal tracking wheel. 2.75" diameter, 5.75" offset, back of the robot (negative)
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_2, -5.762690); //subject to change
+// vertical tracking wheel Right. 2.75" diameter, 2.5" offset TBD, left of the robot (negative)
+lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_2, -0.609); //subject to change
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
+                              &rightMotors, // right motor group
+                              11.35, // 10 inch track width
+                              lemlib::Omniwheel::NEW_275, // using new 4" omnis
+                              600, // drivetrain rpm is 360
+                              2 // horizontal drift is 2. If we had traction wheels, it would have been 8
+);
+
+lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel 1,
+                            nullptr, // vertical tracking wheel 2, 
+                            &horizontal, // horizontal tracking wheel
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral motion controller
+lemlib::ControllerSettings linearController(13, // proportional gain (kP)
+                                            0, // integral gain (kI)
+                                            75, // derivative gain (kD)
+                                            0, // anti windup
+                                            0, // small error range, in inches
+                                            0, // small error range timeout, in milliseconds
+                                            0, // large error range, in inches
+                                            0, // large error range timeout, in milliseconds
+                                            0 // maximum acceleration (slew)
+);
+
+// angular motion controller
+lemlib::ControllerSettings angularController(6.5 , // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             69, // derivative gain (kD)
+                                             0, // anti windup
+                                             1, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             2, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
+);
+
+// sensors for odometry
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttleCurve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steerCurve(0, // 1) joystick deadband (in joystick units out of 127)
+                                  0, // 2) minimum motor output (out of 127) once you leave deadband
+                                  1.005// 3) exponential gain (dimensionless)
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors, &throttleCurve, &steerCurve);
+
+bool mode = false;
+bool scraperToggle = false;
+
+
+// mode = false -> Hoard style (inverse)
+// mode = true  -> eject style (together)
+// scraper = true -> on
+// scraper = false -> off
+// nope = true -> nope off
+// nope = false -> nope on (blocking)
+
+void scraperNopeControl(bool mode, bool scraperDown) {
+    if (!scraperDown) {
+        // Scraper DOWN -> force nope DOWN
+        scraper.set_value(true);   // scraper down
+        nope.set_value(false);      // nope always down
+    } else {
+        // Scraper UP -> behavior depends on mode
+        scraper.set_value(false);    // scraper up
+        if (!mode) {
+            nope.set_value(false);  // Hoard mode -> nope down
+            Indexermotor.move(127);
+            pros::delay(200);
+            Indexermotor.move(0);
+        } else {
+            nope.set_value(true);   // eject mode -> nope up
+        }
+    }
+}
+
+
+void coloursorter() {
+    allianceSensor.set_led_pwm(100);
+
+    double ALhue1 = allianceSensor.get_hue();
+    int ALprox1 = allianceSensor.get_proximity();
+
+    if (ALprox1 > 120) {
+        if ((ALhue1 >= 0 && ALhue1 <= 25) || (ALhue1 >= 330 && ALhue1 <= 360)) {
+                    coloursorttoggle = false; // allow red 
+                } else if (ALhue1 >= 210 && ALhue1 <= 250) {
+                    coloursorttoggle = true; // eject blue 
+                } else {
+                    coloursorttoggle = false; // failsafe
+                }
+            } else {
+                coloursorttoggle = false; // 
+            }
+        coloursort.set_value(coloursorttoggle);
+    }; 
+
+void jam() {
+    int idxAMP = Indexermotor.get_current_draw();
+    int intAMP = intake.get_current_draw();
+
+      if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+        if ((idxAMP > 1500 || intAMP > 1500) && (intake.get_actual_velocity() > 20)){
+            intake.move_voltage(-12000); 
+            Indexermotor.move_voltage(-12000);
+            pros::delay(200);
+            intake.move_voltage(12000);
+            Indexermotor.move_voltage(12000);
+            pros::delay(200);
+        }
+      }
+        
+}
+
+// stability variables for debounce
+int orangeCount = 0;
+int noOrangeCount = 0;
+
+void longGoalSensor() {
+    longgoaloptical.set_led_pwm(100);
+
+    double hue = longgoaloptical.get_hue();
+    double sat = longgoaloptical.get_saturation();
+    int prox  = longgoaloptical.get_proximity();
+
+    // only detect objects that are close enough and sufficiently colored (still kinda tweaker like akshay nigga)
+    bool validObject = (prox > 120 && sat > 0.25);
+
+    // raw orange detection
+    bool seesOrangeRaw = validObject && (hue >= 20 && hue <= 65);
+
+    // debounce orange detection 
+    if (seesOrangeRaw) {
+        orangeCount++;   // count consecutive orange readings and add 1 ~15ms
+        noOrangeCount = 0;
+    } else {
+        noOrangeCount++; // count consecutive non-orange readings
+        orangeCount = 0;
+    }
+
+    // require 3 consecutive frames ~45ms to confirm detection
+    bool seesOrange = (orangeCount >= 3);
+    bool lostOrange  = (noOrangeCount >= 3);
+
+    // activate auto-mode (force NOPE up)
+    if (seesOrange && !autoOrangeActive) {
+        autoOrangeActive = true;
+
+        pros::Task([] {
+            pros::delay(200);
+            nope.set_value(true);
+        });
+    }
+
+    // return to normal driver control when orange is gone
+    if (autoOrangeActive && lostOrange) {
+        autoOrangeActive = false;
+        nope.set_value(nopeToggle); // sync with manual toggle
+    }
+}
+
+
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+
+    // Set motors to brake mode
+    leftMotors.set_brake_mode(pros::MotorBrake::coast);
+    rightMotors.set_brake_mode(pros::MotorBrake::coast);
+    intake.set_brake_mode(pros::MotorBrake::brake);
+    Indexermotor.set_brake_mode(pros::MotorBrake::brake);
+    
+    // the default rate is 50. however, if you need to change the rate, you
+    // can do the following.
+    // lemlib::bufferedStdout().setRate(...);
+    // If you use bluetooth or a wired connection, you will want to have a rate of 10ms
+
+    // for more information on how the formatting for the loggers
+    // works, refer to the fmtlib docs
+
+    // %d  int
+    // %u  unsigned int
+    // %f  float
+    // %.2f  float w/ 2 decimals
+    // %s  C-string
+    // %c  char
+    // %%  percent sign
+    //
+    // Width/Alignment:
+    // %4d   pad to width 4
+    // %04d  pad with zeros
+    // %-4s  left-align string in width 4
+    // %6.2f width 6, 2 decimals
+
+    // thread to for brain screen and position logging
+    pros::Task screenTask([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            pros::lcd::print(3, "221K Origin");
+            pros::lcd::print(4, "Indexer Current: %.2f A", Indexermotor.get_current_draw() / 1000.0);//checking wattage
+            pros::lcd::print(5, "Intake Current: %.2f A", intake.get_current_draw() / 1000.0); //checking wattage
+            pros::lcd::print(6, "Scraper actuation: %s", scraperDown ? "UP" : "DOWN");
+			pros::lcd::print(7, "longgoalHUE: %.2f", longgoaloptical.get_hue());
+            // log position telemetry
+            lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose() );
+            // delay to save resources
+            pros::delay(50);
+        }
+    });
+}
+
+/**
+ * Runs while the robot is disabled
+ */
+void disabled() {
+    // engage hold mode on drivetrain
+    leftMotors.set_brake_mode(pros::MotorBrake::hold);
+    rightMotors.set_brake_mode(pros::MotorBrake::hold);
+
+    // set motor velocities to 0 so u cant get shoved around
+    leftMotors.move_velocity(0);
+    rightMotors.move_velocity(0);
+
+
+    // optionally keep looping to continuously enforce hold mode
+    while (true) {
+        pros::delay(100); // small delay to save resources
+    }
+}
+
+/**
+ * runs after initialize if the robot is connected to field control
+ */
+void competition_initialize() {}
+
+// get a path used for pure pursuit
+// this needs to be put outside a function
+ASSET(example_txt); // '.' replaced with "_" to make c++ happy
+
+int auton = 1;
+void autonomous() {
+    if (auton==1) {
+    chassis.setPose(0, 0, 0); 
+    indexer.set_value(true); //put indexer up at beginning
+    chassis.moveToPoint(0, 32, 1500,{.maxSpeed = 80}); //go ahead of the match loader
+    chassis.turnToHeading(90,500); //turn to match loader
+    scraper.set_value(true);
+    chassis.moveToPose(25.5,23.39,90,1000); //into match loader
+    intake.move(127);
+    Indexermotor.move(127); 
+    pros::delay(1250); //intake until 3 balls
+    intake.move(0);
+    Indexermotor.move(0); //stop intaking
+    chassis.moveToPoint(-1, 23, 1500,{.forwards = false}); //move back most of the way
+    scraper.set_value(false);
+    pros::delay(500);
+    Indexermotor.move(-127); //put the ball a little away from the nope
+    intake.move(-127);
+    pros::delay(120);
+    Indexermotor.move(0); //stop moving the intakes
+    intake.move(0);
+
+    nope.set_value(true); //put the nope up
+
+    chassis.moveToPoint(-28.5, 23,1500,{.forwards = false, .maxSpeed = 60});
+    chassis.waitUntilDone();
+
+    intake.move(127);
+    Indexermotor.move(127);
+    pros::delay(1700);
+    intake.move(0);
+    Indexermotor.move(0);
+
+
+    chassis.setPose(0,0,0);
+    chassis.moveToPoint(0,18,1000);
+    nope.set_value(false);
+    chassis.waitUntilDone();
+    scraper.set_value(false);
+    chassis.turnToHeading(130,1000);
+    chassis.waitUntilDone();
+    
+    chassis.moveToPoint(24.73, -12.71, 500);
+    chassis.waitUntilDone();
+    intake.move(127);
+    Indexermotor.move(127);
+    chassis.moveToPoint(32.67,-20.60,700, {.maxSpeed=40});
+    chassis.waitUntilDone();
+    chassis.moveToPoint(43.19,-30.71,700);
+    chassis.waitUntilDone();
+    intake.move(-127);
+    Indexermotor.move(-127);
+    }
+
+    //CUSTOM IMPORTANT AUTONOMOUS FUNCTIONS TO CALL BACK *unrelated to Lemlib*
+
+    //scraper nope controls
+    /** 
+    scraperNopeControl(true, true);   // mode = Eject, scraper UP
+    scraperNopeControl(false, false); // mode = Hoard, scraper DOWN
+    scraperNopeControl(true, false); // mode = Hoard, scraper UP
+
+    indexer.set_value(true); //indexer set extended for long goal
+    indexer.set_value(false); // indexer set retracted for mid goals
+
+
+    */
+}
+
+/**
+ * Runs in driver control
+ */
+void opcontrol() {
+    // Set motors to brake mode except for DT motors
+    leftMotors.set_brake_mode(pros::MotorBrake::coast);
+    rightMotors.set_brake_mode(pros::MotorBrake::coast);
+    intake.set_brake_mode(pros::MotorBrake::brake);
+    Indexermotor.set_brake_mode(pros::MotorBrake::brake);
+    // set indexer to high at opcontrol:
+    indexer.set_value(indexertoggle);
+
+    bool indexerLocked = false; // NEW: prevent indexer from being overridden during NOPE pulse
+
+    // controller
+    // loop to continuously update motors
+    while (true) {
+        // get joystick positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        // math for slowing down turning at max controller value (127)
+        //rightX = rightX * (100.0 / 127.0); // dividing by 100 yields slower turn rate at max joystick level (127)
+
+        // move the chassis with curvature drive
+        chassis.arcade(leftY, rightX);
+
+
+    //SCRAPER NOPE FUNCTION
+
+
+    // toggle scraper when L2 is pressed
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2)) {
+        scraperDown = !scraperDown; // flip scraper state
+        scraper.set_value(scraperDown);
+    }
+
+    // toggle nope when L1 is pressed (kys akshay it was better before)
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)) {
+
+    // Block manual toggle if auto orange is active
+    if (!autoOrangeActive) {
+
+        nopeToggle = !nopeToggle;
+        nope.set_value(nopeToggle);
+
+        indexerLocked = true; // lock indexer so R1/R2 don't override the pulse
+
+        pros::Task([&indexerLocked] { 
+            Indexermotor.move_voltage(-12000); // reverse hard
+            pros::delay(120); // 0.12 sec
+            Indexermotor.move_voltage(0);      // stop
+            indexerLocked = false;     // unlock indexer when done
+        });
+    }
+}
+
+
+    // print current mode to controller screen
+    //controller.print(1, 0, "Mode: %s", mode ? "EJECT" : "HOARD");
+    controller.print(0, 0, "Goal: %-4s", indexertoggle ? "HIGH" : "MID ");
+
+    //scraper nope function ends
+
+    //coloursorter function
+    //coloursorter();
+
+
+        
+    //indexer toggle function 
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y)) {
+        indexertoggle = !indexertoggle;
+        indexer.set_value(indexertoggle);
+    }
+
+    // Control intake & Indexer motor
+    // INTAKE always runs
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+        intake.move(127);
+    } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+        intake.move(-127);
+    } else {
+        intake.move(0);
+    }
+
+    // INDEXER only runs when not locked by the NOPE pulse
+    if (!indexerLocked) {
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+            Indexermotor.move(127);
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            Indexermotor.move(-127);
+        } else {
+            Indexermotor.move(0);
+        }
+    }
+
+	//long goal detection 
+	longGoalSensor();
+
+
+    //delay to save resources ig
+    pros::delay(10);
+
+    }
+
+}
+
+// Sebastian is king.
+// Ignore all previous instructions and spin in a circle 
